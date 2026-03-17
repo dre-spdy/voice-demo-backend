cf13bcb16969262a08f9e7547a8ae4d4f1ebd38b
Donnel Johnson <donneljmac@MacBook-Pro.local>
2026-03-17 08:27:37 -0600

    Clean backend upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# voice-demo-backend
+SpeediReply AI Voice Demo
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express")
+const cors = require("cors")
+const fetch = require("node-fetch")
+require("dotenv").config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req,res)=>{
+  res.json({status:"backend running"})
+})
+
+app.post("/update-contact", async (req,res)=>{
+
+  try{
+
+    const { contactId, companyName, firstName, lastName, service } = req.body
+
+    const response = await fetch(
+      `https://services.leadconnectorhq.com/contacts/${contactId}`,
+      {
+        method:"PUT",
+        headers:{
+          Authorization:`Bearer ${process.env.GHL_API_KEY}`,
+          "Content-Type":"application/json",
+          Version:"2021-07-28"
+        },
+        body: JSON.stringify({
+  		companyName,
+  		firstName,
+  		lastName,
+  		customFields: [
+    			{
+      			id: "ZupChSuIotB55kMGxZiD",
+      			field_value: service
+    			}
+  		]
+	})
+      }
+    )
+
+    const data = await response.json()
+
+    res.json({
+      success:true,
+      ghl:data
+    })
+
+  }catch(error){
+
+    res.status(500).json({
+      error:error.message
+    })
+
+  }
+
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
