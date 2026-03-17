1a479d04eafe70dbb49cdd137d9dcd3d4dbf128d
Donnel Johnson <donneljmac@MacBook-Pro.local>
2026-03-17 09:34:29 -0600

    Add node-fetch dependency

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.get("/", (req,res)=>{
   res.json({status:"backend running"})
