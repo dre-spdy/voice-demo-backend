0056c5582bade026863b57c070ad597a91f5b51a
Donnel Johnson <donneljmac@MacBook-Pro.local>
2026-04-09 09:47:28 -0600

    update port listener

--- server.js
+++ server.js
@@ -246,7 +246,8 @@ Live demo lead. Speak naturally.
 }
 
 // ===============================
-app.listen(PORT, () => {
+
+app.listen(process.env.PORT || 3000, () => {
   console.log(`✅ Server running on ${PORT}`);
 });
 
